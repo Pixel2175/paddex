f4a3bab269c226720995fb505f6a20f94e4ed821
pi66 <pixel2176@proton.me>
2026-02-07 23:54:58 +0100

    feat: add external JSON data file

--- config.py
+++ config.py
@@ -1,28 +1,16 @@
-about = {
-  "Introduction": "Hi I'm Pixel,I study electrical engineering in high school. I've been into technology since I was a kid, but I only started programming about a year ago, so I'm still pretty new at coding and learning as I go.",
-  "Skills & Languages": "I know Rust, Python, a bit of C, and full-stack web development with Flask, React, and Svelte. My favorite language is definitely Rust. My English level is B2",
-  "Current Situation": "Right now I have less than 3 months to learn and build whatever I want before I have to go back to studying for my BAC exam, which is going to be brutal.",
-  "My Projects": "I've got a few projects I'm working on. There's <a href='/tools/walrs'> walrs</a> that extracts color schemes from wallpapers so you can theme all your apps to match your wallpaper. I also made <a href='/tools/pino-rs'>pino-rs</a>, which is a minimal and customizable notification app written in Rust. Im planning to create another app called Panger, which is basically ranger but written in Rust.",
-  "Philosophy": "My programming philosophy is similar to suckless, but with a twist. I believe you should create software that does what it needs to do, but if you want your computer to look pretty and beautiful or anything else, go for it. Just don't make the core code bloated with unnecessary stuff.",
-  "Music": "I'm really into a mix of genres, especially Japanese music like Malice Mizer, Visual Kei, and J-pop. I love both the raucous and the calm—sometimes in the same track. I also enjoy anime intros and soundtracks, like the music from Frieren. I usually listen locally on my machine using MPV, or directly through my <a href='/music'>music page</a> on this site. Sometimes I stream from YouTube too. Music is always on—whether I'm coding, relaxing, or just thinking.",
-  "My Setup": "I use <a href='https://artixlinux.org'>Artix</a> with <a href='https://dwm.suckless.org/'>dwm</a>, browse with <a href=\"https://floorp.app/en-US\">Floorp</a> and <a href=\"https://qutebrowser.org/\">qutebrowser</a> for web dev, use <a href=\"https://tools.suckless.org/dmenu\">dmenu</a> as my app launcher and <a href=\"https://st.suckless.org/\">st</a> as my terminal. For editing I use <a href=\"https://neovim.io/\">neovim</a>, and when I write notes in markdown files, I have plugins that make them more readable. You can find all my configurations at my <a href=\"https://github.com/pixel2175\">github</a>."
-}
-
-
-
-keywords = {
-        "name":"Walrs",
-        "json":about,
-    }
+from json import load
 
 md_dir       = "./md_files"
 tools_dir    = "./tools"
 pages_dir    = "./pages"
 static_dir   = "./static"
 template_dir = "./static"
+data_file    = "./data.json"
+
+keywords = load(open(data_file,"r"))
 
 filters = [
-# replace       thit       ,with this          , strip p ?
+# replace       thit       ,with this          , strip p?
            ["<p>{% extends",     "{% extends"  , False],
            ["<p>{% block"  ,     "{% block"    , False],
            ["<p><div"      ,     "<div"        , True ],
